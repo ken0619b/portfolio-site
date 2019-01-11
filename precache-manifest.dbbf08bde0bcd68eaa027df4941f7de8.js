22d04a71f3b58f2d2cc3998836df451ef2df6399
Kenichi Ukai <[ken151102@gmail.com]>
2019-01-11 11:39:36 +0900

    initial

---
+++ precache-manifest.dbbf08bde0bcd68eaa027df4941f7de8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4bf65dfaa03696c74ef",
+    "url": "/static/js/main.c4bf65df.chunk.js"
+  },
+  {
+    "revision": "234ca4b841e0bdf6eff6",
+    "url": "/static/js/1.234ca4b8.chunk.js"
+  },
+  {
+    "revision": "c4bf65dfaa03696c74ef",
+    "url": "/static/css/main.4ecf0630.chunk.css"
+  },
+  {
+    "revision": "697936ac4649b0ad89badc3275627e43",
+    "url": "/index.html"
+  }
+];
